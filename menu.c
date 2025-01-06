ad2658b6aa64121ffad1004c7d27857a8611adc8
michal.subert <michal.subert@globesy.sk>
2025-01-06 23:16:58 +0100

    init simulation parameters

--- menu.c
+++ menu.c
@@ -5,6 +5,40 @@
 #include <stdatomic.h>
 #include <string.h>
 
+typedef struct simulation {
+  int world_x;
+  int world_y;
+  int replications;
+  double probabilities[4];
+  int max_steps;
+  char * file_to_save;
+} simulation;
+
+simulation init_simulation_parameters() {
+  simulation sim;
+
+  printf("Zadaj x a y rozmery sveta: \n");
+  scanf(" %d %d", &sim.world_x, &sim.world_y);
+
+  printf("Zadaj pocet replikacii: \n");
+  scanf(" %d", &sim.replications);
+
+  printf("Zadaj pravdepodobnosti posunov chodca, \n");
+  printf("v poradi: HORE, DOLE, VLAVO, VPRAVO \n");
+  scanf(" %lf %lf %lf %lf", &sim.probabilities[0], &sim.probabilities[1], &sim.probabilities[2], &sim.probabilities[3]);
+  //todo check if the sum is 1
+
+  printf("Zadaj maximalny pocet krokov chodca: \n");
+  scanf( " %zu", &sim.max_steps);
+
+  printf("Zadaj meno subora, kde sa ulozi vysledok: \n");
+  scanf(" %99s", &sim.file_to_save);
+
+  //todo add interactive_world, obstacle_world, file_to_load
+
+  return sim;
+}
+
 void print_ascii_menu_screen() {
   printf("==========================================================\n");
   printf("          __  _               _         __               \n");
@@ -39,6 +73,13 @@ int main(int argc, char *argv[])
 
   if (input == 'A' || input == 'a') {
     // todo init simulacie
+    simulation sim = init_simulation_parameters();
+    /* simulation_init(
+     &sim.world_x, &sim.world_y, &sim.replications, &sim.probabilities,
+     &sim.max_steps, &sim.file_to_save,
+     bool interactive_world, bool obstacle_world, char *file_to_load);
+    */
+
   } else if (input == 'B' || input == 'b') {
     // todo pripojenie k existujucej simulacii
   } else if (input == 'C' || input == 'c') {
