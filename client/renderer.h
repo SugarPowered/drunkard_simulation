7def9be1ec2ee5945d7e6b27048df8f349f45a99
BrianMSK <uhrikdenis@gmail.com>
2025-01-12 23:35:04 +0100

    Added funcs to H file

--- client/renderer.h
+++ client/renderer.h
@@ -6,5 +6,9 @@
 #define RENDERER_H
 
 void render_simulation(const char *simulation_state); // Function to draw the current state of the simulation.
+void clear_world();
+void render_world();
+void update_world_from_server(const char *data);
+void run_renderer(int client_socket);
 
 #endif // RENDERER_H
