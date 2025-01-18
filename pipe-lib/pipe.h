d583917a9734c3ffe839bacf133a320f9bb28d30
BrianMSK <uhrikdenis@gmail.com>
2025-01-18 20:54:56 +0100

    fill pipe with template content

--- pipe-lib/pipe.h
+++ pipe-lib/pipe.h
@@ -5,4 +5,10 @@
 #ifndef PIPE_H
 #define PIPE_H
 
+void pipe_init(const char *path);
+void pipe_destroy(const char *path);
+int pipe_open_write(const char *path);
+int pipe_open_read(const char *path);
+void pipe_close(int fd);
+
 #endif //PIPE_H
