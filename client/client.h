59c742f6bfda40d748e6b7c62b9e81175f65b194
BrianMSK <uhrikdenis@gmail.com>
2025-01-18 21:17:22 +0100

    Commented unused function in client

--- client/client.h
+++ client/client.h
@@ -4,9 +4,6 @@
 #include "../sockets-lib/socket.h"
 #include "renderer.h"
 
-#define SERVER_ADDRESS "127.0.0.1"
-
-int initialize_connection();
 void send_to_server(const char *message);
 int receive_from_server(int socket, char *buffer, int buffer_size);
 void close_connection();
